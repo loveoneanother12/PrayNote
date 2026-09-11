39ada98a8440639d6bbd36a52854326f1e1b7a64
Hyunjae Lee <vitsory1@gmail.com>
2026-09-12 03:53:30 +0900

    Refresh PrayNote app icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "praynote-static-v2";
+const CACHE_NAME = "praynote-static-v3";
 const APP_SHELL = ["/manifest.webmanifest", "/icons/icon-192.png", "/icons/apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
